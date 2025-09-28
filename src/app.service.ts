565cffde28a33e57ae19c4450ea38c3dad45caa2
JafirRehman <mjafirrehman@gmail.com>
2025-09-28 22:23:32 +0500

    project is ready now

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
     return 'Hello  People';
+  }
+
+  getBye(): string {
+    return 'Good Bye People';
   }
 }
